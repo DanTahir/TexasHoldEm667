70b376461b37dbc0758be89855a948af2698777a
Varun Narravula <varun@snare.dev>
2024-04-12 11:15:20 -0700

    feat: add nix flake env + direnv

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "CSC-667 Term Project - Poker Webapp";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "csc667-poker";
+          buildInputs = with pkgs; [
+            nodejs_18
+            postgresql # PostgreSQL 15
+          ];
+        };
+      };
+    };
+}
