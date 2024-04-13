73dfaa595a98df3a7ef8de3792208180839a842e
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-12 18:51:04 -0700

    read fn done

--- backend/db/dao/UserDao.ts
+++ backend/db/dao/UserDao.ts
@@ -5,7 +5,12 @@ interface User {
   username: string;
   email: string;
   password: string;
+  balance?: number;
+  profile_image?: string;
 }
+
+//TODO: Promise<Boolean> fn for checking if user exists already in DB.
+//TODO: Delete, Update fn's need to be added
 
 async function createUser(user: User): Promise<boolean> | never {
   try {
@@ -19,5 +24,24 @@ async function createUser(user: User): Promise<boolean> | never {
     return true;
   } catch (error) {
     throw error;
+  }
+
+  async function readUser(
+    username?: string,
+    id?: string,
+  ): Promise<boolean> | never {
+    try {
+      if (!username && !id) {
+        throw new Error("No Parameters to query with.");
+      }
+      const user = await db.one(
+        "SELECT * FROM users WHERE username = $1 OR id = $2",
+        [username || null, id || null],
+      );
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 }
