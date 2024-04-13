f6cb39759a8b1e8e2c4ab9819b3fe69252ef2d6e
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-13 10:09:04 -0700

    add updateUserBalance and updateUserProfile image fn's

--- backend/db/dao/UserDao.ts
+++ backend/db/dao/UserDao.ts
@@ -5,15 +5,17 @@ interface User {
   username: string;
   email: string;
   password: string;
-  balance?: number;
+  balance: number;
   profile_image?: string;
 }
 
-//TODO: Promise<Boolean> fn for checking if user exists already in DB.
 //TODO: Delete, Update fn's need to be added
 
 async function createUser(user: User): Promise<boolean> | never {
   try {
+    if (await readUser(user.username)) {
+      throw new Error("User already exists.");
+    }
     await db.none(
       "INSERT INTO users(username, email, password) VALUES ($1, $2, $3)",
       [user.username, user.password, user.email],
@@ -38,6 +40,50 @@ async function readUser(username?: string, id?: string): Promise<User> | never {
     );
 
     return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUserBalance(
+  username: string,
+  depositAmount: number,
+): Promise<Boolean> | never {
+  try {
+    const user = await readUser(username);
+
+    if (!user) {
+      throw new Error("User doesn't exist.");
+    }
+
+    await db.none("UPDATE users SET balance = $1 WHERE username = $2", [
+      user.balance + depositAmount,
+      username,
+    ]);
+
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateProfileImage(
+  username: string,
+  newProfileImage: string,
+): Promise<Boolean> | never {
+  try {
+    const user = await readUser(username);
+
+    if (!user) {
+      throw new Error("User doesn't exist");
+    }
+
+    await db.none("UPDATE users SET profile_image = $1 WHERE username = $2", [
+      newProfileImage,
+      username,
+    ]);
+
+    return true;
   } catch (error) {
     throw error;
   }
