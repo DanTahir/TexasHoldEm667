d430661ad1f62674fd82a86c9a3bfc2064bec858
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-14 18:50:24 -0700

    remove sessions table from DB and altered default balance to be 10000

---
+++ migrations/1713145539043_remove-sessions-table.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("sessions");
+  pgm.alterColumn("users", "balance", {
+    default: 10000,
+  });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
