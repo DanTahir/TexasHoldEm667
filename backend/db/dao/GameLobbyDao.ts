9fc89fd10ea53eeae29d42a8de8e0d48a00c0dd9
Jake Klopukh <jake.klopukh@gmail.com>
2024-04-15 18:55:10 -0700

    implemented Anthony's changes

--- backend/db/dao/GameLobbyDao.ts
+++ backend/db/dao/GameLobbyDao.ts
@@ -17,72 +17,128 @@ export interface GameLobby {
 }
 
 export async function createLobby(name: String): Promise<String | null> {
-    return await db.one('INSERT INTO game_lobbies (name) VALUES ($1) RETURNING game_lobby_id', [name]);
+    try {
+        return await db.one('INSERT INTO game_lobbies (name) VALUES ($1) RETURNING game_lobby_id', [name]);
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function deleteLobby(game_lobby_id: String): Promise<Boolean | null> {
-    await db.none('DELETE FROM game_lobbies WHERE game_lobby_id=$1', [game_lobby_id]);
-    return true;
+    try {
+        await db.none('DELETE FROM game_lobbies WHERE game_lobby_id=$1', [game_lobby_id]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 } 
 
 export async function getGameLobbyById(lobbyId: String): Promise<GameLobby | null> {
-    return await db.one('SELECT * FROM game_lobbies WHERE game_lobby_id=$1', [lobbyId]);
+    try {
+        return await db.one('SELECT * FROM game_lobbies WHERE game_lobby_id=$1', [lobbyId]);
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function getGameLobbyByName(name: String): Promise<GameLobby | null> {
-    return await db.one('SELECT * FROM game_lobbies WHERE name=$1', [name]);
+    try {
+        return await db.one('SELECT * FROM game_lobbies WHERE name=$1', [name]);
+    } catch (err) {
+        throw err;
+    }
 } 
 
 export async function getRecentGames(): Promise<GameLobby[] | null> {
-    return await db.manyOrNone('SELECT * FROM game_lobbies ORDER BY created_at DESC LIMIT 10');
+    try {
+        return await db.manyOrNone('SELECT * FROM game_lobbies ORDER BY created_at DESC LIMIT 10');
+    } catch (err) {
+        throw err;
+    }
 } 
 
 export async function updateCurrentPlayer(game_lobby_id: String, player_id: String): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET current_player=$2 WHERE game_lobby_id=$1', [game_lobby_id, player_id]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET current_player=$2 WHERE game_lobby_id=$1', [game_lobby_id, player_id]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateDealer(game_lobby_id: String, player_id: String): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET dealer=$2 WHERE game_lobby_id=$1', [game_lobby_id, player_id]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET dealer=$2 WHERE game_lobby_id=$1', [game_lobby_id, player_id]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
-export async function updateGameStage(game_lobby_id: String, game_stage: string): Promise<Boolean| null> {
-    await db.none('UPDATE game_lobbies SET game_stage=$2 WHERE game_lobby_id=$1', [game_lobby_id, game_stage]);
-    return true;
+export async function updateGameStage(game_lobby_id: String, game_stage: string): Promise<Boolean| null> {\
+    try {
+        await db.none('UPDATE game_lobbies SET game_stage=$2 WHERE game_lobby_id=$1', [game_lobby_id, game_stage]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateBuyIn(game_lobby_id: String, buy_in: BigInt): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET buy_in=$2 WHERE game_lobby_id=$1', [game_lobby_id, buy_in]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET buy_in=$2 WHERE game_lobby_id=$1', [game_lobby_id, buy_in]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updatePot(game_lobby_id: String, pot: BigInt): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET pot=$2 WHERE game_lobby_id=$1', [game_lobby_id, pot]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET pot=$2 WHERE game_lobby_id=$1', [game_lobby_id, pot]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateFlops(game_lobby_id: String, flop_1: String, flop_2: String, flop_3: String): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET flop_1=$2, flop_2=$3, flop_3=$4 WHERE game_lobby_id=$1', [game_lobby_id, flop_1, flop_2, flop_3]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET flop_1=$2, flop_2=$3, flop_3=$4 WHERE game_lobby_id=$1', [game_lobby_id, flop_1, flop_2, flop_3]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateRiver(game_lobby_id: String, river: String): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET river=$2 WHERE game_lobby_id=$1', [game_lobby_id, river]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET river=$2 WHERE game_lobby_id=$1', [game_lobby_id, river]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateTurn(game_lobby_id: String, turn: String): Promise<Boolean | null> {
-    await db.none('UPDATE game_lobbies SET turn=$2 WHERE game_lobby_id=$1', [game_lobby_id, turn]);
-    return true;
+    try {
+        await db.none('UPDATE game_lobbies SET turn=$2 WHERE game_lobby_id=$1', [game_lobby_id, turn]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function resetGame(game_lobby_id: String): Promise<Boolean | null> {
-    const RESET_SQL = `
-    UPDATE game_lobbies
-    SET pot=0, call_amount=0, flop_1=NULL, flop_2=NULL, flop_3=NULL, river=NULL, turn=NULL
-    WHERE game_lobby_id=$1`
-
-    await db.none(RESET_SQL, [game_lobby_id]);
-    return true;
+    try {
+        const RESET_SQL = `
+        UPDATE game_lobbies
+        SET pot=0, call_amount=0, flop_1=NULL, flop_2=NULL, flop_3=NULL, river=NULL, turn=NULL
+        WHERE game_lobby_id=$1`
+    
+        await db.none(RESET_SQL, [game_lobby_id]);
+        return true;
+    } catch (err) {
+        throw err
+    }
 }
--- backend/db/dao/PlayerDao.ts
+++ backend/db/dao/PlayerDao.ts
@@ -13,77 +13,126 @@ export interface Player {
 }
 
 export async function createPlayer(player: Player): Promise<String | null> {
-    const CREATE_PLAYER_SQL = 'INSERT INTO players (user_id, game_lobby_id, play_order, status, stake) VALUES ($1, $2, $3, $4, $5) RETURNING player_id';
-    const {user_id, game_lobby_id, play_order, status, stake} = player;
-
-    return await db.one(CREATE_PLAYER_SQL, [user_id, game_lobby_id, play_order, status, stake]);
+    try {
+        const CREATE_PLAYER_SQL = 'INSERT INTO players (user_id, game_lobby_id, play_order, status, stake) VALUES ($1, $2, $3, $4, $5) RETURNING player_id';
+        const {user_id, game_lobby_id, play_order, status, stake} = player;
+    
+        return await db.one(CREATE_PLAYER_SQL, [user_id, game_lobby_id, play_order, status, stake]);
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function getPlayersByLobbyId(game_lobby_id: String): Promise<Player[] | null> {
-    return await db.manyOrNone('SELECT * FROM players WHERE game_lobby_id=$1 ORDER by play_order ASC', [game_lobby_id])
+    try {
+        return await db.manyOrNone('SELECT * FROM players WHERE game_lobby_id=$1 ORDER by play_order ASC', [game_lobby_id])
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function getPlayerByUserAndLobbyId(user_id: String, game_lobby_id: String): Promise<Player | null> {
-    return await db.one('SELECT * FROM players WHERE user_id=$1 AND game_lobby_id=$2', [user_id, game_lobby_id]);
+    try {
+        return await db.one('SELECT * FROM players WHERE user_id=$1 AND game_lobby_id=$2', [user_id, game_lobby_id]);
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function getPlayerById(player_id: String): Promise<Player | null> {
-    return await db.one('SELECT * FROM players WHERE player_id=$1', [player_id])
+    try {
+        return await db.one('SELECT * FROM players WHERE player_id=$1', [player_id])
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function removePlayerByPlayerId(player_id: String): Promise<Boolean | null> {
-    await db.none('DELETE FROM players WHERE player_id=$1', [player_id]);
-    return true;
+    try {
+        await db.none('DELETE FROM players WHERE player_id=$1', [player_id]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function removePlayerByUserAndLobbyId(user_id: String, game_lobby_id: String): Promise<Boolean | null> {
-    await db.none('DELETE FROM players WHERE user_id=$1 AND game_lobby_id=$2', [user_id, game_lobby_id]);
-    return true;
+    try {
+        await db.none('DELETE FROM players WHERE user_id=$1 AND game_lobby_id=$2', [user_id, game_lobby_id]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
+
 }
 
 export async function updateStake(player_id: String, stake: Number): Promise<Boolean | null> {
-    await db.none('UPDATE players SET stake=$2 WHERE player_id=$1', [player_id, stake]);
-    return true;
+    try {
+        await db.none('UPDATE players SET stake=$2 WHERE player_id=$1', [player_id, stake]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateBet(player_id: String, bet: number): Promise<Boolean | null> {
-    await db.none('UPDATE players SET bet=$2 WHERE player_id=$1', [player_id, bet]);
-    return true;
+    try {
+        await db.none('UPDATE players SET bet=$2 WHERE player_id=$1', [player_id, bet]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updatePlayOrder(player_id: String, playOrder: Number): Promise<Boolean | null> {
-    await db.none('UPDATE players SET play_order=$2 WHERE player_id=$1', [player_id, playOrder]);
-    return true;
+    try {
+        await db.none('UPDATE players SET play_order=$2 WHERE player_id=$1', [player_id, playOrder]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateStatus(player_id: String, status: string): Promise<Boolean | null> {
-    await db.none('UPDATE players SET status=$2 WHERE player_id=$1', [player_id, status]);
-    return true;
+    try {
+        await db.none('UPDATE players SET status=$2 WHERE player_id=$1', [player_id, status]);
+        return true;
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updateCards(player_id: String, card_1: String, card_2: String): Promise<Boolean | null> {
-    await db.none('UPDATE players SET card_1=$2, card_2=$3 WHERE player_id=$1', [player_id, card_1, card_2])
-    return true;
+    try {
+        await db.none('UPDATE players SET card_1=$2, card_2=$3 WHERE player_id=$1', [player_id, card_1, card_2])
+        return true;  
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function updatePlayer(player: Player): Promise<Boolean | null> {
-    const {status, stake, bet, play_order, card_1, card_2, player_id} = player;
+    try {
+        const {status, stake, bet, play_order, card_1, card_2, player_id} = player;
 
-    if (player_id == undefined) {
-        throw new Error("player_id is needed to update players");
+        if (player_id == undefined) {
+            throw new Error("player_id is needed to update players");
+        }
+    
+        const UPDATE_PLAYER_SQL = `
+        UPDATE players SET
+            status=$1,
+            stake=$2,
+            bet=$3,
+            play_order=$4,
+            card_1=$5,
+            card_2?=$6
+        WHERE player_id=$7
+        `
+    
+        await db.none(UPDATE_PLAYER_SQL, [status, stake, bet? bet : 0, play_order, card_1? card_1 : null, card_2? card_2 : null, player_id]);
+        return true;
+    } catch (err) {
+        throw err;
     }
-
-    const UPDATE_PLAYER_SQL = `
-    UPDATE players SET
-        status=$1,
-        stake=$2,
-        bet=$3,
-        play_order=$4,
-        card_1=$5,
-        card_2?=$6
-    WHERE player_id=$7
-    `
-
-    await db.none(UPDATE_PLAYER_SQL, [status, stake, bet? bet : 0, play_order, card_1? card_1 : null, card_2? card_2 : null, player_id]);
-    return true;
 }
