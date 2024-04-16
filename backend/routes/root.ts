0808d4cebc8f3f79fe4a6229335f3ecb57756579
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-16 15:26:20 -0700

    add /game GET route

--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -1,11 +1,17 @@
-import express from 'express'
+import express, { Router } from "express";
 
-const router = express.Router();
+const router: Router = express.Router();
 
-router.get('/', (_request, response, _next) => {
-    const name = "Steve";
-    //response.send()
-    response.render('root', {name});
+router.get("/", (_request, response, _next) => {
+  const name = "Steve";
+  //response.send()
+  response.render("root", { name });
+});
+
+router.get("/game", (_request, response, _next) => {
+  response.send("hello");
+  // response.render("game-lobby/game-lobby");
 });
 
 export default router;
+
