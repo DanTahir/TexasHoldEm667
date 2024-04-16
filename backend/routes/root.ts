f11e9a62654e8b0f5f9d7d143dd7c371bf7df128
DanTahir <dtahir@sfsu.edu>
2024-04-16 14:42:45 -0700

    changed const name to const name: string in root.ts

--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -3,13 +3,13 @@ import express from 'express'
 const router = express.Router();
 
 router.get('/', (_request, response, _next) => {
-    const name = "Steve";
+    const name: string = "Steve";
     //response.send()
     response.render('root', {name});
 });
 
 router.get('/chat', (_request, response, _next) => {
-    const name = "Steve";
+    const name: string = "Steve";
     //response.send()
     response.render('chatboxtest', {name});
 });
