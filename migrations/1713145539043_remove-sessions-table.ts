45ad06b0bf413b058a733182cc8d99671c9629f6
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-15 18:44:54 -0700

    remove dropping the sessions table in migrations

--- migrations/1713145539043_remove-sessions-table.ts
+++ migrations/1713145539043_remove-sessions-table.ts
@@ -4,7 +4,6 @@ import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropTable("sessions");
   pgm.alterColumn("users", "balance", {
     default: 10000,
   });
