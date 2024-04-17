486d72a38cc03293ef927e6abb847ab4800a2568
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-17 01:59:30 -0700

    add game render route

---
+++ backend/routes/game.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+
+const router: Router = express.Router();
+
+router.get("/", (_request, response, _next) => {
+  // response.send("hello");
+  response.render("partials/CustomButton", {
+    buttonText: "test",
+  });
+});
+
+export default router;
