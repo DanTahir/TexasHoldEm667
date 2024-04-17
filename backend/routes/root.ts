4d83575a5ca42930e4d0bc90cd2e631264c5f0ef
Varun Narravula <varun@snare.dev>
2024-04-17 13:12:35 -0700

    chore: some miscallaneous DX improvements (CSC-26, CSC-19) (#10)
    * refactor: add extra TS strict options, fix TS errors
    
    * chore: beautify .gitignore
    
    * feat: add prettier for formatting
    
    * feat: add eslint for extra linting rules
    
    * feat: add lefthook for pre-commit validation
    
    * refactor: use import aliases instead of relative paths
    
    * feat: use barrel file for organizing routes
    
    * fix: use @backend namespace instead of @
    
    * chore: actually remove build/server.js

--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -2,17 +2,12 @@ import express from "express";
 
 export const router = express.Router();
 
-
-router.get('/', (_request, response, _next) => {
-    const name: string = "Steve";
-    //response.send()
-    response.render('root', {name});
+router.get("/", (_request, response, _next) => {
+  const name: string = "Steve";
+  response.render("root", { name });
 });
 
-router.get('/chat', (_request, response, _next) => {
-    const name: string = "Steve";
-    //response.send()
-    response.render('chatboxtest', {name});
+router.get("/chat", (_request, response, _next) => {
+  const name: string = "Steve";
+  response.render("chatboxtest", { name });
 });
-
-export default router;
