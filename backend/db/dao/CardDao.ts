5c58abeeea96aa081c93f76c0a4e1020b2d367c2
Jake Klopukh <jake.klopukh@gmail.com>
2024-04-18 15:28:46 -0700

    fix: errors

--- backend/db/dao/CardDao.ts
+++ backend/db/dao/CardDao.ts
@@ -1,31 +1,31 @@
-import db from '../connection';
+import {db} from '../connection.js';
 
 export interface Card {
-    game_card_id?: String
-    game_lobby_id: String,
-    card_id: Number,
-    suit: String,
-    value: Number,
-    shuffled_order: Number
+    game_card_id?: string
+    game_lobby_id: string,
+    card_id: number,
+    suit: string,
+    value: number,
+    shuffled_order: number
 }
 
-export async function createCard(card : Card): Promise<String> {
+export async function createCard(card : Card): Promise<string> {
     return await db.one('INSERT INTO cards (game_lobby_id, card_id, suit, value, shuffled_order) VALUES ($1, $2, $3, $4, $5) RETURNING game_card_id', 
         [card.game_lobby_id, card.card_id, card.suit, card.value, card.shuffled_order]);
 }
 
-export async function getCardByGameCardId(game_card_id: String): Promise<Card> {
+export async function getCardByGameCardId(game_card_id: string): Promise<Card> {
     return await db.one("SELECT * FROM cards WHERE game_card_id=$1", [game_card_id])
 }
 
-export async function getCardsByGame(game_lobby_id: String): Promise<Card[] | null> {
+export async function getCardsByGame(game_lobby_id: string): Promise<Card[] | null> {
     return await db.manyOrNone('SELECT * FROM cards WHERE game_lobby_id=$1 ORDER BY shuffled_order ASC', [game_lobby_id])
 }
 
-export async function deleteCard(game_card_id: String) {
+export async function deleteCard(game_card_id: string) {
     await db.none('DELETE FROM cards WHERE game_card_id=$1', [game_card_id]);
 }
 
-export async function updatePlayOrder(game_card_id: Number, play_order: Number) {
+export async function updatePlayOrder(game_card_id: number, play_order: number) {
     await db.none('UPDATE cards SET play_order=$2 WHERE game_card_id=$1', [game_card_id, play_order]);
 } 
--- backend/db/dao/GameLobbyDao.ts
+++ backend/db/dao/GameLobbyDao.ts
@@ -1,78 +1,122 @@
-import db from '../connection';
+import {db} from "@backend/db/connection.js";
 
 export interface GameLobby {
-    game_lobby_id: String,
-    name: String,
-    game_stage: String,
-    buy_in: BigInt,
-    pot: BigInt,
-    big_blind: BigInt,
-    dealer?: String,
-    current_player?: String,
-    flop_1?: String,
-    flop_2?: String,
-    flop_3?: String,
-    turn?: String,
-    river?: String
+  game_lobby_id: string;
+  name: string;
+  game_stage: string;
+  buy_in: number;
+  pot: number;
+  big_blind: number;
+  dealer?: string;
+  current_player?: string;
+  flop_1?: string;
+  flop_2?: string;
+  flop_3?: string;
+  turn?: string;
+  river?: string;
 }
 
-export async function createLobby(name: String): Promise<String> {
-    return await db.one('INSERT INTO game_lobbies (name) VALUES ($1) RETURNING game_lobby_id', [name]);
+export async function createLobby(name: string): Promise<string> {
+  return await db.one(
+    "INSERT INTO game_lobbies (name) VALUES ($1) RETURNING game_lobby_id",
+    [name],
+  );
 }
 
-export async function deleteLobby(game_lobby_id: String) {
-    await db.none('DELETE FROM game_lobbies WHERE game_lobby_id=$1', [game_lobby_id]);
-} 
-
-export async function getGameLobbyById(lobbyId: String): Promise<GameLobby> {
-    return await db.one('SELECT * FROM game_lobbies WHERE game_lobby_id=$1', [lobbyId]);
+export async function deleteLobby(game_lobby_id: string) {
+  await db.none("DELETE FROM game_lobbies WHERE game_lobby_id=$1", [
+    game_lobby_id,
+  ]);
 }
 
-export async function getGameLobbyByName(name: String): Promise<GameLobby> {
-    return await db.one('SELECT * FROM game_lobbies WHERE name=$1', [name]);
-} 
+export async function getGameLobbyById(lobbyId: string): Promise<GameLobby> {
+  return await db.one("SELECT * FROM game_lobbies WHERE game_lobby_id=$1", [
+    lobbyId,
+  ]);
+}
+
+export async function getGameLobbyByName(name: string): Promise<GameLobby> {
+  return await db.one("SELECT * FROM game_lobbies WHERE name=$1", [name]);
+}
 
 export async function getRecentGames(): Promise<GameLobby[] | null> {
-    return await db.manyOrNone('SELECT * FROM game_lobbies ORDER BY created_at DESC LIMIT 10');
-} 
-
-export async function updateCurrentPlayer(game_lobby_id: String, player_id: String) {
-    await db.none('UPDATE game_lobbies SET current_player=$2 WHERE game_lobby_id=$1', [game_lobby_id, player_id]);
+  return await db.manyOrNone(
+    "SELECT * FROM game_lobbies ORDER BY created_at DESC LIMIT 10",
+  );
 }
 
-export async function updateDealer(game_lobby_id: String, player_id: String) {
-    await db.none('UPDATE game_lobbies SET dealer=$2 WHERE game_lobby_id=$1', [game_lobby_id, player_id]);
+export async function updateCurrentPlayer(
+  game_lobby_id: string,
+  player_id: string,
+) {
+  await db.none(
+    "UPDATE game_lobbies SET current_player=$2 WHERE game_lobby_id=$1",
+    [game_lobby_id, player_id],
+  );
 }
 
-export async function updateGameStage(game_lobby_id: String, game_stage: string) {
-    await db.none('UPDATE game_lobbies SET game_stage=$2 WHERE game_lobby_id=$1', [game_lobby_id, game_stage]);
+export async function updateDealer(game_lobby_id: string, player_id: string) {
+  await db.none("UPDATE game_lobbies SET dealer=$2 WHERE game_lobby_id=$1", [
+    game_lobby_id,
+    player_id,
+  ]);
 }
 
-export async function updateBuyIn(game_lobby_id: String, buy_in: BigInt) {
-    await db.none('UPDATE game_lobbies SET buy_in=$2 WHERE game_lobby_id=$1', [game_lobby_id, buy_in]);
+export async function updateGameStage(
+  game_lobby_id: string,
+  game_stage: string,
+) {
+  await db.none(
+    "UPDATE game_lobbies SET game_stage=$2 WHERE game_lobby_id=$1",
+    [game_lobby_id, game_stage],
+  );
 }
 
-export async function updatePot(game_lobby_id: String, pot: BigInt) {
-    await db.none('UPDATE game_lobbies SET pot=$2 WHERE game_lobby_id=$1', [game_lobby_id, pot]);
+export async function updateBuyIn(game_lobby_id: string, buy_in: number) {
+  await db.none("UPDATE game_lobbies SET buy_in=$2 WHERE game_lobby_id=$1", [
+    game_lobby_id,
+    buy_in,
+  ]);
 }
 
-export async function updateFlops(game_lobby_id: String, flop_1: String, flop_2: String, flop_3: String) {
-    await db.none('UPDATE game_lobbies SET flop_1=$2, flop_2=$3, flop_3=$4 WHERE game_lobby_id=$1', [game_lobby_id, flop_1, flop_2, flop_3]);
+export async function updatePot(game_lobby_id: string, pot: number) {
+  await db.none("UPDATE game_lobbies SET pot=$2 WHERE game_lobby_id=$1", [
+    game_lobby_id,
+    pot,
+  ]);
 }
 
-export async function updateRiver(game_lobby_id: String, river: String) {
-    await db.none('UPDATE game_lobbies SET river=$2 WHERE game_lobby_id=$1', [game_lobby_id, river]);
+export async function updateFlops(
+  game_lobby_id: string,
+  flop_1: string,
+  flop_2: string,
+  flop_3: string,
+) {
+  await db.none(
+    "UPDATE game_lobbies SET flop_1=$2, flop_2=$3, flop_3=$4 WHERE game_lobby_id=$1",
+    [game_lobby_id, flop_1, flop_2, flop_3],
+  );
 }
 
-export async function updateTurn(game_lobby_id: String, turn: String) {
-    await db.none('UPDATE game_lobbies SET turn=$2 WHERE game_lobby_id=$1', [game_lobby_id, turn]);
+export async function updateRiver(game_lobby_id: string, river: string) {
+  await db.none("UPDATE game_lobbies SET river=$2 WHERE game_lobby_id=$1", [
+    game_lobby_id,
+    river,
+  ]);
 }
 
-export async function resetGame(game_lobby_id: String) {
-    const RESET_SQL = `
+export async function updateTurn(game_lobby_id: string, turn: string) {
+  await db.none("UPDATE game_lobbies SET turn=$2 WHERE game_lobby_id=$1", [
+    game_lobby_id,
+    turn,
+  ]);
+}
+
+export async function resetGame(game_lobby_id: string) {
+  const RESET_SQL = `
     UPDATE game_lobbies
     SET pot=0, call_amount=0, flop_1=NULL, flop_2=NULL, flop_3=NULL, river=NULL, turn=NULL
-    WHERE game_lobby_id=$1`
+    WHERE game_lobby_id=$1`;
 
-    await db.none(RESET_SQL, [game_lobby_id]);
+  await db.none(RESET_SQL, [game_lobby_id]);
 }
