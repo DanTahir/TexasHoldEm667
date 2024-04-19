17c9e9c4ffad8a8395a17ee95ed43cb2af5f083d
Varun Narravula <varun@snare.dev>
2024-04-18 17:11:23 -0700

    fix: copy files over from frontend/styles

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,6 +3,8 @@ import * as fs from "fs";
 import * as path from "path";
 import * as url from "url";
 
+import copyStaticFiles from "esbuild-copy-static-files";
+
 const ROOT_PATH = path.dirname(url.fileURLToPath(import.meta.url));
 const FRONTEND_PATH = path.join(ROOT_PATH, "frontend");
 const OUT_PATH = path.join(ROOT_PATH, "backend", "static", "js");
@@ -21,6 +23,18 @@ const CONFIG = {
   sourcemap: dev,
   logLevel: "debug",
   target: "esnext",
+  plugins: [
+    copyStaticFiles({
+      src: "./frontend/styles/",
+      dest: path.join(ROOT_PATH, "backend", "static", "css"),
+      errorOnExist: false,
+      recursive: true,
+      filter: (filename) => {
+        console.log(filename);
+        return filename !== "frontend/styles/main.css";
+      },
+    }),
+  ],
 };
 
 if (dev) {
