fe02d8a8db81c4f8f5eee07779faef6e87fab6f1
anthony <41932917+copbrick@users.noreply.github.com>
2024-04-23 00:00:01 -0700

    feat: Add CreateRequestBody type interface

--- backend/types.ts
+++ backend/types.ts
@@ -3,3 +3,9 @@ import { Request } from "express";
 export interface TypedRequestBody<T> extends Request {
   body: T;
 }
+
+export interface CreateRequestBody extends TypedRequestBody<Body> {
+  name: string;
+  stake: number;
+  user_id: string;
+}
