05cbf5bf4e4ba00037ec44ca449e91be92687def
Jake Klopukh <jake.klopukh@gmail.com>
2024-04-26 21:01:51 -0700

    feat: dialog close click off

--- frontend/index.ts
+++ frontend/index.ts
@@ -2,6 +2,14 @@ const modalDialog = document.getElementById(
   "create-game-modal",
 ) as HTMLDialogElement;
 
+const modalForm = document.getElementById("modal-form") as HTMLFormElement;
+
 document.getElementById("create-game")?.addEventListener("click", () => {
   modalDialog.showModal();
+});
+
+modalDialog.addEventListener("click", (event: Event) => {
+  if (!modalForm.contains(event.target as HTMLElement)) {
+    modalDialog.close();
+  }
 });
