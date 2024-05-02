32c94063ab82c9def0e34b5f1a8dc8f462b09aba
Varun Narravula <varun@snare.dev>
2024-05-02 15:34:42 -0700

    fix: use .item() instead of array indexing for seats list

--- frontend/messages/playerJoin.ts
+++ frontend/messages/playerJoin.ts
@@ -5,7 +5,7 @@ const roomID = (document.getElementById("room-id") as HTMLInputElement).value;
 
 export function handle(socket: Socket) {
   socket.on(`game:join:${roomID}`, ({ playOrder, player }) => {
-    const seat = seats[playOrder - 1];
+    const seat = seats.item(playOrder - 1);
 
     const new_seat = seat.cloneNode(true) as HTMLDivElement;
     const button = new_seat.querySelector("button")!;
