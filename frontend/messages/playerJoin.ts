89d24fb2f944ba769bbf15da0cbc39e8b67f60fa
DanTahir <dtahir@sfsu.edu>
2024-05-03 18:11:10 -0700

    resolved the join button issue in an ugly way, hoping someone has a better fix, still getting the quit button discoloration issue

--- frontend/messages/playerJoin.ts
+++ frontend/messages/playerJoin.ts
@@ -1,10 +1,11 @@
 import { Socket } from "socket.io-client";
 
-const seats = document.querySelectorAll(".seat");
 const roomID = (document.getElementById("room-id") as HTMLInputElement).value;
 
 export function handle(socket: Socket) {
   socket.on(`game:join:${roomID}`, ({ playOrder, player, stake }) => {
+    console.log("Hello from player join socket, playOrder = " + playOrder);
+    const seats = document.querySelectorAll(".seat");
     const seat = seats.item(playOrder - 1);
 
     const new_seat = seat.cloneNode(true) as HTMLDivElement;
--- frontend/messages/playerQuit.ts
+++ frontend/messages/playerQuit.ts
@@ -13,7 +13,28 @@ export function handle(socket: Socket) {
     const button = new_seat.querySelector("button")!;
     new_seat.classList.add("empty-seat");
     button.textContent = `Join`;
+
+    const handler = () => {
+      button.textContent = "Joining...";
+      button.removeEventListener("click", handler);
+
+      fetch(`/game/${roomID}/join`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          playOrder: playOrder,
+        }),
+      }).then(async (res) => {
+        if (!res.ok) {
+          button.textContent = "Join";
+          button.addEventListener("click", handler);
+          alert(await res.text());
+        }
+      });
+    };
+
+    button.addEventListener("click", handler);
+
     seat.parentNode?.replaceChild(new_seat, seat);
-    //joinHandle();
   });
 }
