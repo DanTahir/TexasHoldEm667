bf91b769d8dd7647a20dfc8b3ce5e9e6f80152bb
DanTahir <dtahir@sfsu.edu>
2024-05-10 13:30:32 -0700

    made settings button a way to reset your balance (in case you go broke)

--- backend/routes/home.ts
+++ backend/routes/home.ts
@@ -1,24 +1,54 @@
 import { getRecentGames } from "@backend/db/dao/GameLobbyDao";
+import { 
+  updateUserBalance,
+  readUserFromID,
+} 
+from "@backend/db/dao/UserDao";
 import { Views } from "@backend/views";
-import express, { Router } from "express";
+import express, { Router, Request } from "express";
 
 export const router: Router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get("/", async (req: Request, res, next) => {
   let games;
 
   try {
     games = await getRecentGames();
   } catch (error) {
     next(error);
+
+
   }
+
+  const userName = req.session.user.username;
+  const userID = req.session.user.id;
+  let user;
+  let balance = req.session.user.balance;
+  try {
+    user = await readUserFromID(userID);
+  } catch(error){
+    next(error);
+  }
+  balance = user?.balance;
+
+  console.log(`Hello from home - balance: ${balance}`);
 
   if (req.session.form) {
     const { message, name, stake } = req.session.form;
     req.session.form = undefined;
 
-    res.render(Views.Home, { games, message, name, stake });
+    res.render(Views.Home, { games, userName, balance, message, name, stake });
   } else {
-    res.render(Views.Home, { games });
+    res.render(Views.Home, { games, userName, balance });
   }
+});
+
+router.post("/resetBalance", async (req: Request, res) => {
+  const userName = req.session.user.username;
+  await updateUserBalance(userName, 10000);
+  console.log(`Hello from resetBalance - userName: ${userName}`);
+  res.redirect('/home')
+  
+
+
 });
