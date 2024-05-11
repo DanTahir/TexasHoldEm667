d089e3fc0ff95e6d5a8b060e487ca2440b85662e
DanTahir <dtahir@sfsu.edu>
2024-05-11 13:47:22 -0700

    fixed quitgame running on wrong page which was causing chat to fail on home

--- frontend/events/quitGames.ts
+++ frontend/events/quitGames.ts
@@ -1,26 +1,30 @@
 export function handle() {
-  const quitButtonDivNull = document.querySelector(".quit-button");
-  const quitButtonDiv =
-    quitButtonDivNull ? quitButtonDivNull : new HTMLDivElement();
-  const quitButtonNull = quitButtonDiv.querySelector("button");
-  const quitButton = quitButtonNull ? quitButtonNull : new HTMLButtonElement();
-  const handler = () => {
-    quitButton.textContent = "Quitting...";
-    const gameID = document.location.pathname.split("/")[2];
+  const url = document.location.pathname.split("/")[1];
+  if (url === "game") {
+    const quitButtonDivNull = document.querySelector(".quit-button");
+    const quitButtonDiv =
+      quitButtonDivNull ? quitButtonDivNull : new HTMLDivElement();
+    const quitButtonNull = quitButtonDiv.querySelector("button");
+    const quitButton =
+      quitButtonNull ? quitButtonNull : new HTMLButtonElement();
+    const handler = () => {
+      quitButton.textContent = "Quitting...";
+      const gameID = document.location.pathname.split("/")[2];
 
-    fetch(`/game/${gameID}/quit`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        playOrder: 1,
-      }),
-    }).then(async (res) => {
-      quitButton.textContent = "Quit Game";
-      if (!res.ok) {
-        alert(await res.text());
-      }
-    });
-  };
+      fetch(`/game/${gameID}/quit`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          playOrder: 1,
+        }),
+      }).then(async (res) => {
+        quitButton.textContent = "Quit Game";
+        if (!res.ok) {
+          alert(await res.text());
+        }
+      });
+    };
 
-  quitButton.addEventListener("click", handler);
+    quitButton.addEventListener("click", handler);
+  }
 }
